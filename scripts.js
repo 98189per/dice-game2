582fcb04ee682e470072c6a0be92822002ad1586
98189per <98189per@wcdsbcloud.ca>
2019-12-08 23:28:09 -0500

    data validation and player order

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
+var searchParams;
 var noplayers;
 var players = [];
 var gametext;
-var playerdatasubmit;
 var playerdatainput = [];
 var playertypeinput = [];
+var typebuffer;
+var turn;
+var roll = new Object();
 
 function startGame(){
     var paramString = window.location.search;
-    var searchParams = new URLSearchParams(paramString);
+    searchParams = new URLSearchParams(paramString);
     gametext = document.getElementById("gametext");
     if(searchParams.has("noplayers")){
         noplayers = searchParams.get("noplayers");
         gametext.innerHTML += "Hello! Would you like to play dice game?";
+        gametext.innerHTML += "</br>" + "Players take turns rolling dice and flipping a coin to try to get a score as close to 50 without going over.";
+        gametext.innerHTML += "</br>" + "Each round, the winner gets a certain number of points added to their overall score.";
+        gametext.innerHTML += "</br>" + "The first player to reach a total score of 1000 wins the game!";
         document.getElementsByClassName("gamebutton")[0].innerHTML = "Yes";
         document.getElementsByClassName("gamebutton")[1].innerHTML = "No";
         gametext.innerHTML += "</br>" + "Okay! Let's get started, how many players are there?";
@@ -19,32 +25,28 @@ function startGame(){
         document.getElementsByClassName("gamebutton")[1].id = "nctive";
         getPlayerData(noplayers);
         if(searchParams.has("playerdata0")){
-            playerdatasubmit.setAttribute("type","button");
             var i;
             for(i=0;i<noplayers;i++){
                 players[i] = new Object();
                 playerdatainput[i].setAttribute("value",searchParams.get("playerdata"+i));
                 players[i].name = searchParams.get("playerdata"+i);
                 if(searchParams.get("type"+i)=="person"){
-                    playertypeinput[2*i-1].setAttribute("checked","checked");
+                    playertypeinput[2*i+1].setAttribute("checked","checked");
                     players[i].type = "person";
                 }
                 else{
-                    playertypeinput[2*i].setAttribute("checked","checked");
+                    playertypeinput[2*i+2].setAttribute("checked","checked");
                     players[i].type = "computer";
                 }
             }
-            gametext.innerHTML += "okay!";
-            for(i=0;i<noplayers;i++){
-                gametext.innerHTML += "</br>" + players[i].name + " - " + players[i].type;
-            }
-            /*
-            this is where I left off, (got player info, etc.)
-            now connect back into actual game
-            */
+            gametext.innerHTML += "Time to play the game!";
+            playerOrder();
         }
     }else{
         gametext.innerHTML += "Hello! Would you like to play dice game?";
+        gametext.innerHTML += "</br>" + "Players take turns rolling dice and flipping a coin to try to get a score as close to 50 without going over.";
+        gametext.innerHTML += "</br>" + "Each round, the winner gets a certain number of points added to their overall score.";
+        gametext.innerHTML += "</br>" + "The first player to reach a total score of 1000 wins the game!";
         document.getElementsByClassName("gamebutton")[0].innerHTML = "Yes";
         document.getElementsByClassName("gamebutton")[1].innerHTML = "No";
     }
@@ -58,6 +60,7 @@ function updateScroll(){
 function getPlayerData(noplayers){
     var playerdataform = document.createElement("FORM");
     playerdataform.setAttribute("method","get");
+    playerdataform.setAttribute("id","playerdataform");
     var askplayertext = document.createTextNode("No. of players: ");
     askplayerinput = document.createElement("INPUT");
     askplayerinput.setAttribute("type","text");
@@ -75,43 +78,103 @@ function getPlayerData(noplayers){
         playerdatainput[i] = document.createElement("INPUT");
         playerdatainput[i].setAttribute("type","text");
         playerdatainput[i].setAttribute("name","playerdata"+i);
-        playertypeinput[2*i-1] = document.createElement("INPUT");
-        playertypeinput[2*i-1].setAttribute("type","radio");
-        playertypeinput[2*i-1].setAttribute("name","type"+i);
-        playertypeinput[2*i-1].setAttribute("value","person");
-        playertypeinput[2*i] = document.createElement("INPUT");
-        playertypeinput[2*i].setAttribute("type","radio");
-        playertypeinput[2*i].setAttribute("name","type"+i);
-        playertypeinput[2*i].setAttribute("value","computer");
+        playertypeinput[2*i+1] = document.createElement("INPUT");
+        playertypeinput[2*i+1].setAttribute("type","radio");
+        playertypeinput[2*i+1].setAttribute("name","type"+i);
+        playertypeinput[2*i+1].setAttribute("value","person");
+        playertypeinput[2*i+2] = document.createElement("INPUT");
+        playertypeinput[2*i+2].setAttribute("type","radio");
+        playertypeinput[2*i+2].setAttribute("name","type"+i);
+        playertypeinput[2*i+2].setAttribute("value","computer");
         playerdataform.appendChild(document.createTextNode("Player name: "));
         playerdataform.appendChild(playerdatainput[i]);
-        playerdataform.appendChild(playertypeinput[2*i-1]);
+        playerdataform.appendChild(playertypeinput[2*i+1]);
         playerdataform.appendChild(document.createTextNode("Person "));
-        playerdataform.appendChild(playertypeinput[2*i]);
+        playerdataform.appendChild(playertypeinput[2*i+2]);
         playerdataform.appendChild(document.createTextNode("Computer "));
         playerdataform.appendChild(document.createElement("BR"));
     }
     playerdatasubmit = document.createElement("INPUT");
-    playerdatasubmit.setAttribute("type","submit");
+    playerdatasubmit.setAttribute("type","button");
+    playerdatasubmit.setAttribute("onclick","submitForm(playerdataform,playertypeinput)");
     playerdatasubmit.setAttribute("value","Done");
     playerdataform.appendChild(playerdatasubmit);
     gametext.appendChild(playerdataform);
 }
 
+function isFormComplete(radiobutton){
+    if(radiobutton.value=="person"){
+        typebuffer=radiobutton;
+        return true;
+    }
+    else
+        return typebuffer.checked || radiobutton.checked;
+}
+
 function getNoPlayers(){
     var playerform = document.createElement("FORM");
     playerform.setAttribute("method","get");
+    playerform.setAttribute("id","playerform");
+    playerform.setAttribute("onkeydown","return event.key != 'Enter';");
     var askplayertext = document.createTextNode("No. of players: ");
     askplayerinput = document.createElement("INPUT");
     askplayerinput.setAttribute("type","text");
     askplayerinput.setAttribute("name","noplayers");
     askplayersubmit = document.createElement("INPUT");
-    askplayersubmit.setAttribute("type","submit");
+    askplayersubmit.setAttribute("type","button");
+    askplayersubmit.setAttribute("onclick","submitForm(playerform,askplayerinput)");
     askplayersubmit.setAttribute("value","Enter");
     playerform.appendChild(askplayertext);
     playerform.appendChild(askplayerinput);
     playerform.appendChild(askplayersubmit);
     gametext.appendChild(playerform);
+}
+
+function submitForm(formToSubmit,dataToValidate){
+    if(dataToValidate.name=="noplayers"){
+        if(dataToValidate.value==parseInt(dataToValidate.value,10) && dataToValidate.value>1)
+            formToSubmit.submit();
+        else
+            alert("Please enter a valid no. of players");
+    }else if(searchParams.has("playerdata0")!=true){
+        if(dataToValidate.every(isFormComplete))
+            formToSubmit.submit();
+        else
+            alert("Please check every player type");
+    }
+}
+
+function playerOrder(){
+    gametext.innerHTML += "</br>" + "Let's roll dice to see who goes first!";
+    var i;
+    for(i=0;i<noplayers;i++){
+        rollDice();
+        players[i].order = roll.a + roll.b;
+        gametext.innerHTML += "</br>" + players[i].name + " rolled a " + roll.a + " and a " + roll.b + "!";
+    }
+    players.sort(function(a,b){return (a.order<b.order ? 1 : -1);});
+    while(players[0].order==players[1].order){
+        gametext.innerHTML += "</br>" + "Since " + players[0].name + " and " + players[1].name + " rolled the same sum, they must re-roll!";
+        rollDice();
+        players[0].order = roll.a + roll.b;
+        gametext.innerHTML += "</br>" + players[0].name + " rolled a " + roll.a + " and a " + roll.b + "!";
+        rollDice();
+        players[1].order = roll.a + roll.b;
+        gametext.innerHTML += "</br>" + players[1].name + " rolled a " + roll.a + " and a " + roll.b + "!";
+        players.sort(function(a,b){return (a.order<b.order ? 1 : -1);});
+    }
+    gametext.innerHTML += "</br>" + players[0].name + " goes first, let the game begin!!!";
+    handleGameFlow();
+}
+
+function handleGameFlow(){
+
+}
+
+function rollDice(){
+    roll.a = Math.floor(Math.random() * 6) + 1;
+    roll.b = Math.floor(Math.random() * 6) + 1;
+    return roll;
 }
 
 function switchFunction(button){
@@ -130,48 +193,12 @@ function switchFunction(button){
                 document.getElementsByClassName("gamebutton")[0].id = "nctive";
                 document.getElementsByClassName("gamebutton")[1].style.display = "none";
                 break;
-            case "First":
-                gametext.innerHTML += "</br>" + "Play the game by rolling dice until you choose to stop" + "</br>" + "The first player to go over 50 loses" + "</br>" + "If you get exactly 50 you win!";
-                document.getElementsByClassName("gamebutton")[0].innerHTML = "Roll";
-                document.getElementsByClassName("gamebutton")[1].innerHTML = "End";
-                break;
-            case "Second":
-                break;
             case "Roll":
-                gametext.innerHTML += "</br>" + "You rolled a " + rollDice("player") + ", your score is now " + player.score + "</br>" + "Would you like to roll again or end your turn?";
+                gametext.innerHTML += "</br>" + "You rolled a ";
                 break;
             case "End":
                 gametext.innerHTML += "</br>" + "Now it's my turn!";
                 break;
         }
-    }
-}
-
-function rollDice(obj){
-    var num = Math.floor(Math.random() * 6) + 1;
-    if(obj == "player")
-        player.score += num;
-    else{
-        computer.turnscores.push(num);
-        computer.score += num;
-    }
-   return num;
-}
-
-function computerRoll(tolerance){
-    computer.turnscores = [];
-    while(computer.score < tolerance)
-        rollDice();
-}
-
-function winningConditions(){
-    if(player.score>50){
-        gametext.innerHTML += "</br>" + "You rolled over 50 and lost! The current standings are now, ";
-    }else if(computer.score>50){
-
-    }else if(player.score==50){
-
-    }else if(computer.score==50){
-
     }
 }
